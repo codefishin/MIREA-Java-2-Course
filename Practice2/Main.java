42a3d29c302a2dfe92e71e889d9fb03e55514596
stomp <150149808+stomp666@users.noreply.github.com>
2024-09-10 13:16:17 +0300

    Create Main.java

---
+++ Practice2/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Enter model, license, color and year.");
+        Scanner input = new Scanner(System.in);
+
+        String model = input.nextLine();
+        String license = input.nextLine();
+        String color = input.nextLine();
+        int year = input.nextInt();
+        Car car_full = new Car(model, license, color, year);
+        Car car_null = new Car();
+        Car car_half = new Car(model, year);
+
+        System.out.println("\nInfo about car with all info\n");
+        car_full.To_String();
+        System.out.println("\nInfo about car with half the info\n");
+        car_half.To_String();
+        System.out.println("\nInfo about car with no info\n");
+        car_null.To_String();
+
+        System.out.println("Please change the current year (" + year + ")");
+
+        year = input.nextInt();
+        car_full.Modify_Year(year);
+        car_full.To_String();
+    }
+}
