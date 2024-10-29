3398a88b8a25c21cbf22b61033116c74b8e6cbb4
stomp <150149808+stomp666@users.noreply.github.com>
2024-10-29 14:47:43 +0300

    Create IH8STACKS.java

---
+++ Practice9/IH8STACKS.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class IH8STACKS<E> extends ArrayList<E> {
+    private java.util.ArrayList<E> list = new java.util.ArrayList<>();
+    public int getSize() {
+        return list.size();
+    }
+    public E peek() {
+        return list.get(getSize() - 1);
+    }
+    public void push(E o) {
+        list.add(o);
+    }
+    public E pop() {
+        E o = list.get(getSize() - 1);
+        list.remove(getSize() - 1);
+        return o;
+    }
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+    @Override
+    public String toString() {
+        return "стек: " + list.toString();
+    }
+
+}
